ada6eb1f8170b6c049853a7c596581ef7b881f05
IsaiahSama <zeldaplayergl@gmail.com>
2020-08-28 10:29:10 -0400

    Finished setting up Main.py

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-bot = commands.Bot(command_prefix="><", case_insensitive=True)
+bot = commands.Bot(command_prefix=">>>", case_insensitive=True)
 bot.help_command = None
+
+bot.load_extension("objcom")
+bot.load_extension("myhelp")
 
 @bot.event
 async def on_ready():
     print("And we're in")
-    activity = discord.Activity(name="><help", type=discord.ActivityType.unknown)
+    activity = discord.Activity(name=">>>help", type=discord.ActivityType.unknown)
     await bot.change_presence(activity=activity)
 
-yes =os.getenv("key")
+yes=os.getenv("key")
 bot.run(yes)
